a021b316d595ad89c7f11fcf11d4d5e8b3c728b3
siva-yarra <85125666+siva-yarra@users.noreply.github.com>
2021-11-09 17:15:14 +0530

    project

---
+++ src/main/java/project/ConnectionProvider.java
@@ -0,0 +1,19 @@
+package project;
+import java.sql.*;
+public class ConnectionProvider {
+	
+public static Connection getCon()
+{
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project1","root","78@Akeshyarra");
+		return con;
+	}
+	catch( Exception e) 
+	{
+		System.out.println(e);
+		return null;
+		
+	}
+}
+}
